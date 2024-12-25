f3146e228d61ca8f2ef425c2d709d30ac91eaefa
Gabriel Henrique <gabrielhenriquegh3878@gmail.com>
2024-12-24 21:52:35 -0300

    feat: add HomeAluno page in frontend and its stylesheet

--- client/src/components/livro/HomeAluno.tsx
+++ client/src/components/livro/HomeAluno.tsx
@@ -1,7 +1,69 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import '../../styles/livro/HomeAluno.css';
+
+interface Livro {
+  idLivro: number;
+  titulo: string;
+  autor: string;
+  capa: string;
+  editora: string;
+  ano_publicacao: number;
+  genero: string;
+  idProfessor: number;
+  createdAt: number;
+  updatedAt: number;
+}
 
 function HomeAluno() {
+  const [livros, setLivros] = useState<Livro[]>([]);  
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const fetchLivros = async () => {
+      try{
+        const token = localStorage.getItem('token');
+        const response = await axios.get<Livro[]>("http://localhost:4000/livros", {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+        
+        setLivros(response.data);
+        console.log(response.data); 
+      }catch(error) {
+        setError("was not poossible")
+        console.log(error);
+      } 
+    };
+    fetchLivros();
+  }, [])
+
   return (
-    <div>HomeAluno</div>
+    <div className="homeAluno">
+    <h2>Livros Disponíveis</h2>
+    {error && <p className="error">{error}</p>}
+    {livros.length > 0 ? (
+      <div className="livro-container">
+        {livros.map((livro) => (
+          <div className="livro-card" key={livro.idLivro}>
+            <img
+              src={`http://localhost:4000/uploads/${livro.capa}`} // Altere se necessário para o caminho correto das imagens.
+              alt={livro.titulo}
+              className="livro-capa"
+              onLoad={() => console.log('Image loaded successfully')}
+            />
+            <h3>{livro.titulo}</h3>
+            <p>Autor: {livro.autor}</p>
+            <p>Editora: {livro.editora}</p>
+            <p>Ano: {livro.ano_publicacao}</p>
+            <p>Gênero: {livro.genero}</p>
+          </div>
+        ))}
+      </div>
+    ) : (
+      <p>Nenhum livro cadastrado.</p>
+    )}
+  </div>
   )
 }
 
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import router from './routes/route';
+import path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'public', 'uploads')));
 app.use(router)
 
 export default app;
